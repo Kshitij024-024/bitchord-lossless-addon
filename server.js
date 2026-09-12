a7185af50942d758687d3285b368471f6fa50279
Kshitij024-024 <meshramkshitij.024@gmail.com>
2026-09-12 21:44:35 +0530

    Refactor server.js to use URL for routing

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const http = require("http");
+const { URL } = require("url");
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "application/json");
 
-  if (req.url === "/") {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  // Main server page
+  if (url.pathname === "/") {
     res.end(JSON.stringify({
       name: "BitChord Lossless Addon",
       status: "running"
     }));
-  } 
-  else if (req.url === "/manifest.json") {
+  }
+
+  // Add-on manifest
+  else if (url.pathname === "/manifest.json") {
     res.end(JSON.stringify({
       id: "bitchord-lossless",
       name: "BitChord Lossless Addon",
@@ -19,7 +25,19 @@ const server = http.createServer((req, res) => {
       description: "Lossless music resolver for BitChord",
       resources: ["search", "stream"]
     }));
-  } 
+  }
+
+  // Search endpoint
+  else if (url.pathname === "/search") {
+    const query = url.searchParams.get("q") || "";
+
+    res.end(JSON.stringify({
+      query: query,
+      tracks: []
+    }));
+  }
+
+  // Not found
   else {
     res.statusCode = 404;
     res.end(JSON.stringify({
